73c25e2e2b25f6a8a7e852240e6f209be8741827
CindyBischof <cindy.bischof@gmail.com>
2018-08-13 06:07:26 -0500

    test for menu toggle

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -65,6 +65,11 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('visibility toggles upon click', function(){
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+          });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
